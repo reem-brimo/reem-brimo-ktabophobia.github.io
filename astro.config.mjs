fdf81395e627beab139ba23297184623ad12ab23
Reem Brimo <reembrimo@gmail.com>
2026-03-26 21:58:35 +0300

    Add github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://reem-brimo.github.io/reem-brimo-ktabophobia.github.io/',  // Update this to your actual domain
   output: 'static',
   integrations: [sitemap()],
 });
