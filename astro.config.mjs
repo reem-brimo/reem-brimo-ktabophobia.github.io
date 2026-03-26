0a88882bd608e4addb68309b2fe8407f0aa490d8
Reem Brimo <reembrimo@gmail.com>
2026-03-26 22:48:05 +0300

    Fix path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://reem-brimo.github.io/reem-brimo-ktabophobia.github.io/',
+  site: 'https://reem-brimo.github.io/reem-brimo-ktabophobia.github.io/', // domain + repo path
+  base: '/reem-brimo-ktabophobia.github.io/', // the path the site is served under
   output: 'static',
   integrations: [sitemap()],
 });
